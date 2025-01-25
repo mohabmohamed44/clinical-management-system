624cf9bbee952c5a59c5771bcd05647c5721daad
Mohab Mohammed <92528415+mohabmohamed44@users.noreply.github.com>
2025-01-25 20:58:31 +0200

    Initial Commit..

---
+++ src/Pages/NotFound/NotFound.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect} from "react";
+import Style from "./NotFound.module.scss";
+export default function NotFound() {
+  return (
+    <>
+     <h2>Template Name</h2>
+    </>
+  );
+}
