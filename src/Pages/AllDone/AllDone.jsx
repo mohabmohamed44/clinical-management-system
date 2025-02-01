ba48bc9360b04076f2971a993f0273dadc10ce42
Mohab Mohammed <92528415+mohabmohamed44@users.noreply.github.com>
2025-02-01 16:44:01 +0200

    Page added Successfully..

--- src/Pages/AllDone/AllDone.jsx
+++ src/Pages/AllDone/AllDone.jsx
@@ -11,7 +11,7 @@ export default function AllDone() {
         <p className="font-semibold text-gray-500 text-xl">
           Your Password has been reset Successfully.
         </p>
-        <button className="w-1/3 text-white bg-[#11319e] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center mt-4">
+        <button className="w-1/3 text-white bg-[#11319e] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-3 text-center mt-4">
           <Link to="/login">Go to Login</Link>
         </button>
       </div>
