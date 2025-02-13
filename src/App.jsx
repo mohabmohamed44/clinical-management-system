10d4bd66d7c82b8fafe68ed658cef25905d4bd52
Mohab Mohammed <92528415+mohabmohamed44@users.noreply.github.com>
2025-02-13 18:22:42 +0200

    React-query Added..

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,55 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css';
-import Login from './Pages/Login/Login';
-import Layout from './Pages/Layout/Layout'
-import Register from './Pages/Register/Register';
-import Home from './Pages/Home/Home';
-import ForgotPassword from './Pages/ForgotPassword/ForgotPassword';
-import ResetCode from './Pages/ResetCode/ResetCode';
-import UpdatePassword from './Pages/UpdatePassword/UpdatePassword';
-import NotFound from './Pages/NotFound/NotFound';
-import AllDone from './Pages/AllDone/AllDone';
-import Contact from './Pages/Contact/Contact';
-import Appointments from './Pages/Appointments/Appointments';
-import FindDoctor from './Pages/FindDoctor/FindDoctor';
-import { Toaster } from 'react-hot-toast';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import "./App.css";
+import Login from "./Pages/Login/Login";
+import Layout from "./Pages/Layout/Layout";
+import Register from "./Pages/Register/Register";
+import Home from "./Pages/Home/Home";
+import ForgotPassword from "./Pages/ForgotPassword/ForgotPassword";
+import ResetCode from "./Pages/ResetCode/ResetCode";
+import UpdatePassword from "./Pages/UpdatePassword/UpdatePassword";
+import NotFound from "./Pages/NotFound/NotFound";
+import AllDone from "./Pages/AllDone/AllDone";
+import Contact from "./Pages/Contact/Contact";
+import Appointments from "./Pages/Appointments/Appointments";
+import FindDoctor from "./Pages/FindDoctor/FindDoctor";
+
+// React Query Client
+const queryClient = new QueryClient();
 
 // routing
 const router = createBrowserRouter([
   {
-    path:"",
+    path: "",
     element: <Layout />,
     children: [
-      {path: "", index: true, element:<Home/>},
-      {path: "login", element:<Login/>},
-      {path: "register", element:<Register/>},
-      {path: "forgot_password", element: <ForgotPassword/>},
-      {path: "reset_code", element: <ResetCode/>},
-      {path: "update_password", element: <UpdatePassword/>},
-      {path: "all_done", element: <AllDone/>},
-      {path: "contact", element: <Contact/>},
-      {path: "appointments", element: <Appointments/>},
-      {path: "find_doctor", element: <FindDoctor/>},
-      {path: "*", element: <NotFound/>}
+      { path: "", index: true, element: <Home /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      { path: "forgot_password", element: <ForgotPassword /> },
+      { path: "reset_code", element: <ResetCode /> },
+      { path: "update_password", element: <UpdatePassword /> },
+      { path: "all_done", element: <AllDone /> },
+      { path: "contact", element: <Contact /> },
+      { path: "appointments", element: <Appointments /> },
+      { path: "find_doctor", element: <FindDoctor /> },
+      { path: "*", element: <NotFound /> },
     ],
-  }
-])
+  },
+]);
 function App() {
   // Browser router
   return (
     <>
-      <RouterProvider router={router} />
-      <Toaster position="top-center" reverseOrder={false}/>
-    </> 
-  )
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <Toaster position="top-center" reverseOrder={false} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
