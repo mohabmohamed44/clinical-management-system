7ec943df865b65d125aa2dcd830eb0b443214524
mohabmohamed44 <mohamedmohab742@gmail.com>
2025-04-21 18:33:56 +0200

    Done Minifying css and js

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,56 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import compression from 'vite-plugin-compression';
-import cssnanoPlugin from 'cssnano';
-// https://vite.dev/config/
+import cssnano from 'cssnano';
+
 export default defineConfig({
   plugins: [
-    react(), 
+    react(),
     tailwindcss(),
-    compression(),
-    cssnanoPlugin(),
+    compression({
+      algorithm: 'brotliCompress', // or 'gzip'
+      ext: '.br',
+    }),
   ],
-  test:{
-    globals: true,
-    environment: 'jsdom',
+  css: {
+    postcss: {
+      plugins: [
+        cssnano({
+          preset: 'default',
+        })
+      ]
+    }
   },
-  optimizeDeps: {
-    include:["react-helmet-async"],
-  },
-  build:{
-    minify: false,
-    terserOptions:{
-      compress:{
+  build: {
+    minify: 'terser', // or 'esbuild' for faster minification
+    cssMinify: 'esbuild', // Use esbuild for CSS minification
+    terserOptions: {
+      compress: {
         drop_console: true,
         pure_funcs: ['console.info', 'console.debug'],
+      },
+      format: {
+        comments: false
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
       }
     }
+  },
+  optimizeDeps: {
+    include: ["react-helmet-async"],
+    esbuildOptions: {
+      minify: true
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
   }
 })
